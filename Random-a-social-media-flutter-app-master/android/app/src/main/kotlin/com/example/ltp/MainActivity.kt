8d196816e77d7bf5e434987e5978bfb963109e1f
div910 <32384044+div910@users.noreply.github.com>
2023-07-04 07:19:52 +0530

    Delete Random-a-social-media-flutter-app-master directory

--- Random-a-social-media-flutter-app-master/android/app/src/main/kotlin/com/example/ltp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ltp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Random-a-social-media-flutter-app-master/lib/utils/constants.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color ktxtColor = Colors.black;
-MaterialColor? kmainColor = Colors.indigo;
-const Color kMainColor = Colors.indigo;
-Color kaccentColor = Colors.indigoAccent;
-Color kbgColor = Colors.white.withOpacity(0.9);
-Color kmaintxtColor = Colors.black;
-Color ktxtwhiteColor = Colors.white;
